a7cb2913cf823a8e0f27d4f1ecc880e05481c906
Belthunder <chmball3@gmail.com>
2022-07-21 16:52:13 -0700

    Update counter.py
    Laid out the basic framework of the program, including the socket setup

--- counter.py
+++ counter.py
@@ -6,4 +6,24 @@ import time
 import zmq 
 
 def main() :
-    return 0
+    
+    context = zmq.Context()
+    
+    # set uop the zmq socket to talk to the requester:
+    print("setting up socket to connect to link-counting service")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:56443")
+    
+    
+    while True:
+        #wait for a request from the link-counting client
+        message = socket.recv()
+        print("Recieved request: %s" % message)
+        
+        # do the work of translating the JSON into a usable list, 
+        # counting duplicates, creating a map, and converting that map to a JSON object to send back.
+    
+        #TO DO
+        
+        # Send the completed map back to the client as a JSON object
+        socket.send()
