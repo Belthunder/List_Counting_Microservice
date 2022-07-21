5cea253c00d75555c65931a534e0dc1acdcd9b47
Belthunder <chmball3@gmail.com>
2022-07-20 22:28:14 -0700

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,6 @@
+# counter.py written by Christopher Ball as part of the list-counting microservice
+# for OSU CS 361 Summer Term 2022, Ashley McLemore's Link-Counter service
+# last modified 07/20/2022
+
+def main() :
+    return 0
