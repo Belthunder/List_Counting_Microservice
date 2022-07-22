4dd71f4d5977e6366cefe9546e10e88373affc9b
Belthunder <chmball3@gmail.com>
2022-07-22 11:54:06 -0700

    Beginning to work

--- counter.py
+++ counter.py
@@ -3,20 +3,18 @@
 # last modified 07/20/2022
 
 import time
-import zmq 
+import zmq
 
-def main() :
-    
+
+def main():
     context = zmq.Context()
-    
+
     # set up the zmq socket to talk to the requester:
     print("setting up socket to connect to link-counting service")
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:56443")
-    
-    
+    socket.bind("tcp://*:62243")
+
     while True:
-        
         #  Wait for next request from client
         message = socket.recv()
         print(f"Received request: {message}")
@@ -26,30 +24,20 @@ def main() :
 
         #  Send reply back to client
         socket.send_string("World")
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        #wait for a request from the link-counting client
-       # message = socket.recv_json()
-       # print("Recieved request: %s" % message)
-        
-        # do the work of translating the JSON into a usable list, 
-        # counting duplicates, creating a map, and converting that map to a JSON object to send back.
-        
-      #  links_list = message["links"]
-        #create the map to send
-       # map = {}
-        # count duplicates
-      #  for i in links_list:
-      #      map[i] = links_list.count(i)
-        
-        # convert the map to JSON and send the completed map back to the client as a JSON object
-      #  socket.send_json(map)
+
+        # wait for a request from the link-counting client
+    # message = socket.recv_json()
+    # print("Recieved request: %s" % message)
+
+    # do the work of translating the JSON into a usable list,
+    # counting duplicates, creating a map, and converting that map to a JSON object to send back.
+
+    #  links_list = message["links"]
+    # create the map to send
+    # map = {}
+    # count duplicates
+    #  for i in links_list:
+    #      map[i] = links_list.count(i)
+
+    # convert the map to JSON and send the completed map back to the client as a JSON object
+    #  socket.send_json(map)
