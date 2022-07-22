d77efd1b39d366b639b2a436916c936b48dbc612
Belthunder <chmball3@gmail.com>
2022-07-22 12:18:57 -0700

    It works!?
    cleaned up indentation and syntax. It seems like it works!

--- counter.py
+++ counter.py
@@ -2,7 +2,6 @@
 # for OSU CS 361 Summer Term 2022, Ashley McLemore's Link-Counter service
 # last modified 07/20/2022
 
-import time
 import zmq
 
 
@@ -15,29 +14,19 @@ def main():
     socket.bind("tcp://*:62243")
 
     while True:
-        #  Wait for next request from client
-        message = socket.recv()
-        print(f"Received request: {message}")
-
-        #  Do some 'work'
-        time.sleep(1)
-
-        #  Send reply back to client
-        socket.send_string("World")
-
         # wait for a request from the link-counting client
-    # message = socket.recv_json()
-    # print("Recieved request: %s" % message)
+        message = socket.recv_json()
+        print("Received request: %s" % message)
 
-    # do the work of translating the JSON into a usable list,
-    # counting duplicates, creating a map, and converting that map to a JSON object to send back.
+        # do the work of translating the JSON into a usable list,
+        # counting duplicates, creating a map, and converting that map to a JSON object to send back.
 
-    #  links_list = message["links"]
-    # create the map to send
-    # map = {}
-    # count duplicates
-    #  for i in links_list:
-    #      map[i] = links_list.count(i)
+        links_list = message["links"]
+        # create the map to send
+        counting = {}
+        # count duplicates
+        for i in links_list:
+            counting[i] = links_list.count(i)
 
-    # convert the map to JSON and send the completed map back to the client as a JSON object
-    #  socket.send_json(map)
+        # convert the map to JSON and send the completed map back to the client as a JSON object
+        socket.send_json(counting)
