65c287059b8b3895fe5ea09c135ab2a1dff2f110
Belthunder <chmball3@gmail.com>
2022-07-22 16:26:32 -0700

    Upadated test client
    Moved some variables to constants to make customization easier

--- counter.py
+++ counter.py
@@ -4,6 +4,10 @@
 
 import zmq
 
+FINAL_SOCKET_END = "tcp://*:62243"
+END_MESSAGE = "complete"
+LIST_TOPIC = "links"
+
 
 def main():
     context = zmq.Context()
@@ -11,17 +15,21 @@ def main():
     # set up the zmq socket to talk to the requester:
     print("setting up socket to connect to link-counting service")
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:62243")
+    socket.bind(FINAL_SOCKET_END)
 
     while True:
         # wait for a request from the link-counting client
         message = socket.recv_json()
         print("Received request: %s" % message)
 
+        # check to see if the ending message has been sent
+        if message[LIST_TOPIC] == END_MESSAGE:
+            break
+
         # do the work of translating the JSON into a usable list,
         # counting duplicates, creating a map, and converting that map to a JSON object to send back.
 
-        links_list = message["links"]
+        links_list = message[LIST_TOPIC]
         # create the map to send
         counting = {}
         # count duplicates
